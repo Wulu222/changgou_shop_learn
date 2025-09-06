feaf0209cfabe8902520bc0d6e86f9ce4cfce91a
wulux <3392294949@qq.com>
2025-09-07 05:27:34 +0800

    框架搭建

---
+++ changgou_service/changgou_service_goods/src/main/java/com/changgou/service/goods/Democontroller.java
@@ -0,0 +1,16 @@
+package com.changgou.service.goods;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/demo")
+@RestController // @Controller + @ResponseBody
+public class Democontroller {
+
+    @GetMapping("/test")
+    public String hello() {
+        return "hello";
+    }
+}
